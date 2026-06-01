5a746b8a3e4a72dc3d70cc3402198e77e9eaccdf
numnum5 <junseopy5315@gmail.com>
2026-06-01 21:18:49 +1000

    Update g++ version and create a server.

--- api/server.cpp
+++ api/server.cpp
@@ -7,11 +7,29 @@
 #include <string>
 #include <thread>
 
-#include "glaze/glaze.hpp"
+#include <glaze/glaze.hpp>
 
 // #include "cpu.hpp"
 #include "emulator.hpp"
 // #include "compiler.hpp"
+
+struct CpuState
+{
+    int pc;
+    int cycle;
+};
+
+template<>
+struct glz::meta<CpuState>
+{
+    using T = CpuState;
+
+    static constexpr auto value =
+        object(
+            "pc", &T::pc,
+            "cycle", &T::cycle
+        );
+};
 
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
@@ -44,20 +62,20 @@ int main()
 
         // intialise 
         
-        // Emulator emu;
+        Emulator emu;
         
-        // emu.load_elf("firmware.elf");
-        // emu.startCpu();
+        emu.load_elf("firmware.elf");
+        emu.startCpu();
 
 
         while (true)
         {
             ws.read(buffer);
-
+            // glz::write().value_or("error");
             std::string message = beast::buffers_to_string(buffer.data());
 
             buffer.consume(buffer.size());
-
+            
             std::cout
                 << "Received: "
                 << message
@@ -65,8 +83,17 @@ int main()
 
             if (message == "step")
             {
+                emu.cpu.step();
+                CpuState state{
+                    .pc = emu.cpu.fetch_pc,
+                    .cycle = emu.cpu.cycle
+                };
+                
+                auto json = glz::write_json(state);
                 // cpu.stepInstruction();
-                ws.write(net::buffer("instruction stepped"));
+                ws.text(true); // send as text websocket frame
+
+                ws.write(net::buffer(json.value()));
             }
             else if (message == "cycle")
             {
